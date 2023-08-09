bdf573d2a1fb1dc7c351c2625d71cf4c47980098
Lans12345 <olanalans12345@gmail.com>
2023-08-09 20:12:23 +0800

    added config

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Carnab Admin Panel',
+      debugShowCheckedModeBanner: false,
       home: LoginScreen(),
     );
   }
