cdb20654ef4e4fbce37e88f4b06f33aa8d40e3d8
Lans12345 <olanalans12345@gmail.com>
2023-09-02 11:31:29 +0800

    added analytics tab

--- lib/screens/home_tabs/analytics_tab.dart
+++ lib/screens/home_tabs/analytics_tab.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 import '../../widgets/text_widget.dart';
 
 class AnalyticsTab extends StatelessWidget {
@@ -219,24 +220,67 @@ class AnalyticsTab extends StatelessWidget {
           const SizedBox(
             height: 10,
           ),
-          Card(
-            elevation: 5,
-            child: Container(
-              height: 250,
-              width: 800,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: const Padding(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [],
-                ),
-              ),
-            ),
-          ),
+          StreamBuilder<QuerySnapshot>(
+              stream:
+                  FirebaseFirestore.instance.collection('Reports').snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  print(snapshot.error);
+                  return const Center(child: Text('Error'));
+                }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  print('waiting');
+                  return const Padding(
+                    padding: EdgeInsets.only(top: 20),
+                    child: Center(
+                        child: CircularProgressIndicator(
+                      color: Colors.black,
+                    )),
+                  );
+                }
+
+                final data = snapshot.requireData;
+                return Card(
+                  elevation: 5,
+                  child: Container(
+                    height: 400,
+                    width: 800,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: SizedBox(
+                          height: 425,
+                          child: FlutterMap(
+                            options: MapOptions(
+                              center: LatLng(13.401972, 123.345909),
+                              zoom: 12.0,
+                            ),
+                            children: [
+                              TileLayer(
+                                urlTemplate:
+                                    'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                                userAgentPackageName: 'com.example.app',
+                              ),
+                              CircleLayer(
+                                circles: [
+                                  for (int i = 0; i < data.docs.length; i++)
+                                    CircleMarker(
+                                      color: Colors.red.withOpacity(0.3),
+                                      point: LatLng(data.docs[i]['lat'],
+                                          data.docs[i]['long']),
+                                      radius: 20,
+                                    ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        )),
+                  ),
+                );
+              }),
         ])));
   }
 }
